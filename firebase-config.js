ecfffab0f0f5d9b7fcd832686d4637d051f477a8
robertjuniorrj85-ux <robertjunior.rj85@gmail.com>
2026-09-06 13:52:24 -0300

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,1 @@
-// Cole aqui as configurações do seu projeto Firebase.
-// Quando preencher este arquivo, o app passa a usar login e banco online.
-// Enquanto estiver vazio, o aplicativo funciona em modo demonstração/local.
-
-export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
-};
+export const firebaseConfig={apiKey:"",authDomain:"",projectId:"",storageBucket:"",messagingSenderId:"",appId:""};
--- sw.js
+++ sw.js
@@ -1,5 +1,1 @@
-
-const CACHE="mvl-v1";
-const ASSETS=["./","./index.html","./styles.css","./app.js","./firebase-config.js","./manifest.json","./assets/logo-mvl.jpg"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C='mvl-v2.1';const A=['./','./index.html','./styles.css','./app.js','./manifest.json','./assets/logo-mvl.jpg','./assets/icon-192.png','./assets/icon-512.png'];self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(A)))});self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{let x=r.clone();caches.open(C).then(c=>c.put(e.request,x));return r}).catch(()=>caches.match(e.request))));
