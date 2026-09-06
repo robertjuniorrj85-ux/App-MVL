e65e570717752c43e6335523cad2ae41e9500913
robertjuniorrj85-ux <robertjunior.rj85@gmail.com>
2026-09-06 13:58:54 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,5 @@
-const C='mvl-v2.1';const A=['./','./index.html','./styles.css','./app.js','./manifest.json','./assets/logo-mvl.jpg','./assets/icon-192.png','./assets/icon-512.png'];self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(A)))});self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{let x=r.clone();caches.open(C).then(c=>c.put(e.request,x));return r}).catch(()=>caches.match(e.request))));
+const C='mvl-v3';
+const A=['./','./index.html','./styles.css','./app.js','./manifest.json','./firebase-config.js','./logo-mvl.jpg','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(A)))});
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{let x=r.clone();caches.open(C).then(c=>c.put(e.request,x));return r}).catch(()=>caches.match(e.request))));
