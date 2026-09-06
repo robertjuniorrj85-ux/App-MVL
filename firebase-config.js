e7f543deb4d67dbca343febb3b776cea842c5c4b
robertjuniorrj85-ux <robertjunior.rj85@gmail.com>
2026-09-06 13:42:02 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Cole aqui as configurações do seu projeto Firebase.
+// Quando preencher este arquivo, o app passa a usar login e banco online.
+// Enquanto estiver vazio, o aplicativo funciona em modo demonstração/local.
+
+export const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
---
+++ sw.js
@@ -0,0 +1,5 @@
+
+const CACHE="mvl-v1";
+const ASSETS=["./","./index.html","./styles.css","./app.js","./firebase-config.js","./manifest.json","./assets/logo-mvl.jpg"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
