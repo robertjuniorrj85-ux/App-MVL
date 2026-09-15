940e81256c28f55e69e7a19db29788cc4ac409f5
robertjuniorrj85-ux <robertjunior.rj85@gmail.com>
2026-09-14 21:12:51 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
-const CACHE='mvl-v9-6-3';
+const CACHE='mvl-v10-0';
 const STATIC=[
-  './index.html?v=9.6.3','./styles.css?v=9.6.3','./app.js?v=9.6.3','./firebase-config.js?v=9.6.3',
-  './manifest.webmanifest?v=9.6.3','./logo-mvl.jpg','./mvl-icon-192-v4.png','./mvl-icon-512-v4.png'
+  './index.html?v=10.0','./styles.css?v=10.0','./app.js?v=10.0','./firebase-config.js?v=10.0',
+  './manifest.webmanifest?v=10.0','./logo-mvl.jpg','./mvl-icon-192-v4.png','./mvl-icon-512-v4.png'
 ];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
